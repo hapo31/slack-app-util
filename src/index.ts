eea15faaef83e8db80047f8eefe0c0fe4e5ea8f7
happou31 <happo31@gmail.com>
2019-07-28 01:42:42 +0900

    initial commit.

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import fetch from "node-fetch";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`listen to port:${port}`);
+});
+
+app.post("/post/image", async (req, res, next) => {
+  const url: string | undefined = req.query.url;
+  const title: string | undefined = req.query.title;
+
+  if (url) {
+    const imageUrl = decodeURIComponent(url);
+
+    const postData = {
+      response_type: "in_channel",
+      text: "",
+      attachments: [
+        {
+          fields: [
+            {
+              title: title ? decodeURIComponent(title) : "image"
+            }
+          ],
+          image_url: imageUrl
+        }
+      ]
+    };
+    res.status(200);
+    res.json(postData);
+  }
+});
