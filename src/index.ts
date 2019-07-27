280fbe90df9ad9f9338319eb97decfc296c67fae
happou31 <happo31@gmail.com>
2019-07-28 02:21:18 +0900

    fix: サービスポートをちゃんと設定してなかったです…

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = 3000;
+const port = parseInt(process.env.PORT || "3000");
 
 const server = app.listen(port, () => {
   console.log(`listen to port:${port}`);
