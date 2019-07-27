20aad8e3358158d95f41277b1d1478a2aaa3c966
happou31 <happo31@gmail.com>
2019-07-28 01:57:36 +0900

    fix: なんかコンパイル通らんかった、キレそう

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
-import express from "express";
-import bodyParser from "body-parser";
+import express = require("express");
+import bodyParser = require("body-parser");
 
 const app = express();
 
